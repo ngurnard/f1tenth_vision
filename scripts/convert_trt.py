25f695b1f4c7f42db21e91c5369378f6ed216afa
Mana5a-Sathyan <89954326+Mana5a-Sathyan@users.noreply.github.com>
2023-04-12 19:11:35 -0400

    Update fp16

--- scripts/convert_trt.py
+++ scripts/convert_trt.py
@@ -7,62 +7,7 @@ import os
 # logger to capture errors, warnings, and other information during the build and inference phases
 TRT_LOGGER = trt.Logger()
 
-
-# def get_engine(onnx_file_path, engine_file_path=""):
-#     """Attempts to load a serialized engine if available, otherwise builds a new TensorRT engine and saves it."""
-
-#     def build_engine():
-#         """Takes an ONNX file and creates a TensorRT engine to run inference with"""
-#         EXPLICIT_BATCH = 1 << (int)(trt.NetworkDefinitionCreationFlag.EXPLICIT_BATCH)
-#         # network = builder.create_network(explicit_batch)
-#         builder = trt.Builder(TRT_LOGGER)
-#         network = builder.create_network(EXPLICIT_BATCH)
-#         config = builder.create_builder_config() 
-#         parser = trt.OnnxParser(network, TRT_LOGGER)
-#         runtime = trt.Runtime(TRT_LOGGER)
-#         # with trt.Builder(TRT_LOGGER) as builder, builder.create_network(
-#         #     common.EXPLICIT_BATCH
-#         # ) as network, builder.create_builder_config() as config, trt.OnnxParser(
-#         #     network, TRT_LOGGER
-#         # ) as parser, trt.Runtime(
-#         #     TRT_LOGGER
-#         # ) as runtime:
-#         config.max_workspace_size = 1 << 28  # 256MiB
-#         builder.max_batch_size = 1
-#         # Parse model file
-#         if not os.path.exists(onnx_file_path):
-#             print(
-#                 "ONNX file {} not found, please run yolov3_to_onnx.py first to generate it.".format(onnx_file_path)
-#             )
-#             exit(0)
-            
-#         print("Loading ONNX file from path {}...".format(onnx_file_path))
-#         with open(onnx_file_path, "rb") as model:
-#             print("Beginning ONNX file parsing")
-#             if not parser.parse(model.read()):
-#                 print("ERROR: Failed to parse the ONNX file.")
-#                 for error in range(parser.num_errors):
-#                     print(parser.get_error(error))
-#                 return None
-            
-#         network.get_input(0).shape = [1, 3, 180, 320]
-#         print("Completed parsing of ONNX file")
-#         print("Building an engine from file {}; this may take a while...".format(onnx_file_path))
-#         plan = builder.build_serialized_network(network, config)
-#         engine = runtime.deserialize_cuda_engine(plan)
-#         print("Completed creating Engine")
-#         with open(engine_file_path, "wb") as f:
-#             f.write(plan)
-#         return engine
-
-#     if os.path.exists(engine_file_path):
-#         # If a serialized engine exists, use it instead of building an engine.
-#         print("Reading engine from file {}".format(engine_file_path))
-#         with open(engine_file_path, "rb") as f, trt.Runtime(TRT_LOGGER) as runtime:
-#             return runtime.deserialize_cuda_engine(f.read())
-#     else:
-#         return build_engine()
-    
+   
 
 def get_engine(onnx_file_path, engine_file_path=""):
     """Attempts to load a serialized engine if available, otherwise builds a new TensorRT engine and saves it."""
@@ -85,9 +30,8 @@ def get_engine(onnx_file_path, engine_file_path=""):
         # ) as runtime:
         config.max_workspace_size = 1 << 28  # 256MiB
         builder.max_batch_size = 1
-        # if config.use_fp16:
-        # 	builder_config.set_flag(trt.BuilderFlag.FP16)
-        
+        # For FP16
+        # self.config.set_flag(trt.BuilderFlag.FP16)
         # Parse model file
         if not os.path.exists(onnx_file_path):
             print(
